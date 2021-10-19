31c887057563a0dec5a619b1c2cfa2d5b7af5f72
Asher Hancock <ajh172@pitt.edu>
2021-10-19 06:28:41 -0400

    logistic regression.

---
+++ Logistic_Regression/ex2_main.m
@@ -0,0 +1,102 @@
+
+%% Part 1 %%
+
+% Load Data
+% The first two columns contain the exam scores and the third column contains the label.
+data = load('ex2data1.txt');
+X = data(:, [1, 2]); 
+y = data(:, 3);
+
+figure(1)
+% Plot the data with + indicating (y = 1) examples and o indicating (y = 0) examples.
+plotData(X, y);
+ 
+% Labels and Legend
+xlabel('Exam 1 score')
+ylabel('Exam 2 score')
+
+% Specified in plot order
+legend('Admitted', 'Not admitted')
+
+%% Compute logistic regression
+%  Setup the data matrix appropriately
+[m, n] = size(X);
+
+% Add intercept term to X
+X = [ones(m, 1) X];
+
+% Initialize the fitting parameters
+initial_theta = zeros(n + 1, 1);
+
+% Compute and display the initial cost and gradient
+[cost, grad] = costFunction(initial_theta, X, y);
+fprintf('Cost at initial theta (zeros): %f\n', cost);
+
+%  Set options for fminunc
+options = optimoptions(@fminunc,'Algorithm','Quasi-Newton','GradObj', 'on', 'MaxIter', 400);
+
+%  Run fminunc to obtain the optimal theta
+%  This function will return theta and the cost 
+[theta, cost] = fminunc(@(t)(costFunction(t, X, y)), initial_theta, options);
+
+% Plot Boundary
+plotDecisionBoundary(theta, X, y);
+% Add some labels 
+hold on;
+% Labels and Legend
+xlabel('Exam 1 score')
+ylabel('Exam 2 score')
+% Specified in plot order
+legend('Admitted', 'Not admitted')
+hold off;
+
+%%
+
+%% Part 2
+%  The first two columns contains the X values and the third column
+%  contains the label (y).
+data = load('ex2data2.txt');
+X = data(:, [1, 2]); y = data(:, 3);
+
+plotData(X, y);
+% Put some labels 
+hold on;
+% Labels and Legend
+xlabel('Microchip Test 1')
+ylabel('Microchip Test 2')
+% Specified in plot order
+legend('y = 1', 'y = 0')
+hold off;
+
+% Add Polynomial Features
+% Note that mapFeature also adds a column of ones for us, so the intercept term is handled
+X = mapFeature(X(:,1), X(:,2));
+
+% Initialize fitting parameters
+initial_theta = zeros(size(X, 2), 1);
+
+% Set regularization parameter lambda to 1
+lambda = 1;
+
+% Compute and display initial cost and gradient for regularized logistic regression
+[cost, grad] = costFunctionReg(initial_theta, X, y, lambda);
+fprintf('Cost at initial theta (zeros): %f\n', cost);
+
+% Compute and display cost and gradient with all-ones theta and lambda = 10
+test_theta = ones(size(X,2),1);
+[cost, grad] = costFunctionReg(test_theta, X, y, 10);
+fprintf('\nCost at test theta (with lambda = 10): %f\n', cost);
+
+% Optimize
+[theta, J, exit_flag] = fminunc(@(t)(costFunctionReg(t, X, y, lambda)), initial_theta, options);
+
+% Plot Boundary
+plotDecisionBoundary(theta, X, y);
+% Add some labels 
+hold on;
+% Labels and Legend
+xlabel('Exam 1 score')
+ylabel('Exam 2 score')
+% Specified in plot order
+legend('Admitted', 'Not admitted')
+hold off;
